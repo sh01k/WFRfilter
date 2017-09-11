d6b411c9917aa9e6a94b12bff1606ff529e327b3
Shoichi Koyama <koyama@invite3.institut-langevin.local>
2017-09-11 20:07:24 +0200

    modify spherical harmonic func

--- lib/sph_harm_mat.m
+++ lib/sph_harm_mat.m
@@ -1,22 +1,30 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% (c) Shoichi Koyama (koyama.shoichi@ieee.org) 2016.09.02
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (c) Shoichi Koyama 2015.07.31
+% Generate matrix of spherical harmonic functions
+% USAGE "sph_harm_mat(Nmax,theta,phi)"
+% INPUT
+%   Nmax     Maximum order  
+%   theta    Zenith angle
+%   phi      Azimuth angle
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [Y] = sph_harm_mat(Nmax,theta,phi)
 
 Y = zeros(Nmax+1,2*Nmax+1);
 
-n = 0:Nmax;
+n = 0:1:Nmax;
+size_n = length(n);
 
-for in = 1:(Nmax+1)
-    m = -n(in):n(in);
+for in=1:size_n
+    m = -n(in):1:n(in);
+    size_m = length(m);
     
-    Pn = legendre(n(in),cos(theta));
+    %Associated Legendre function
+    Pnm = legendre(n(in),cos(theta));
+    Pnm_vec = [Pnm(n(in)+1:-1:2); Pnm].';
+    
+    sgn = [(-1).^abs(m(1:n(in))),ones(1,n(in)+1)];
+    Y(in,1:size_m) = sgn.*sqrt((2*n(in)+1)/(4*pi).*factorial(n(in)-abs(m))./factorial(n(in)+abs(m))).*Pnm_vec.*exp(1i*m*phi);
+end
 
-    for im = 1:(2*n(in)+1)
-        mp = abs(m(im));
-        Pnm = Pn(mp+1);
-        
-        Y(in,im) = sqrt(((2.*n(in)+1)./(4.*pi)).*(factorial(n(in)-mp)./factorial(n(in)+mp))).*Pnm.*exp(1i.*m(im).*phi);
-    end
 end
